7936d89db4a9aaf11be92847c3a42f90dc17876b
DeepikaGoli <deepika3203@gmail.com>
2021-11-10 10:27:44 +0530

    first commit

---
+++ Deepika/evenodd.java
@@ -0,0 +1,10 @@
+public class evenodd {
+    public static void main(String[] args)
+    {
+        int n=10;
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+    }
+}
